8830968c67ce930105f0d288207bbbeed5b3dafb
sam <54972161+samiam308@users.noreply.github.com>
2020-06-02 16:00:21 -0700

    Create gma.c

---
+++ gma.c
@@ -0,0 +1,89 @@
+#include "gma.h"
+
+#include <string.h>
+#include <errno.h>
+#include <endian.h>
+
+static inline int bufcpy(const void **begin, const void *end,
+	void *dest, size_t count){
+
+	if(end - *begin < count) return -1;
+	memcpy(dest, *begin, count);
+	*begin += count;
+	return 0;
+}
+
+static inline const char *bufstr(const void **begin, const void *end){
+	const void *const b = *begin;
+	const size_t sl = strnlen(b, end - b);
+	if(((const char *) b)[sl] != '\0') return NULL;
+
+	*begin += sl;
+	return b;
+}
+
+static inline int skip_null(const void **begin, const void *end){
+	if(*begin + 1 >= end) return -1;
+	*begin += 1;
+	return 0;
+}
+
+static int _gma_header_parse(struct gma_header *hdr, const void *_buf, size_t count){
+	const void *begin = _buf;
+	const void *end = begin + count;
+
+	const void *it = begin;
+
+	//load hdr->ident and make sure it contains "GMAD"
+	if(bufcpy(&it, end, hdr->ident, 4)) return -ENODATA;
+	if(memcmp(hdr->ident, "GMAD", 4) != 0) return -EPROTO;
+
+	//load version and make sure its supported
+	if(bufcpy(&it, end, &hdr->version, 1)) return -ENODATA;
+	if(hdr->version > GMA_MAX_VERSION) return -ENOTSUP;
+
+	if(bufcpy(&it, end, &hdr->steamid, 8)) return -ENODATA;
+	if(bufcpy(&it, end, &hdr->timestamp, 8)) return -ENODATA;
+
+	//fun fact: gmad doesnt do endian swapping for its integers. but i will!
+	hdr->steamid = le64toh(hdr->steamid);
+	hdr->timestamp = le64toh(hdr->timestamp);
+
+	//version 1 and later have a string called "content" so ill read it out
+	if(hdr->version > 1){
+		hdr->content = bufstr(&it, end);
+		if(hdr->content == NULL) return -ENODATA;
+		if(skip_null(&it, end)) return -ENODATA;
+	} else hdr->content = NULL;
+
+	//load name
+	hdr->name = bufstr(&it, end);
+	if(hdr->name == NULL) return -ENODATA;
+	if(skip_null(&it, end)) return -ENODATA;
+
+	//load desc
+	hdr->desc = bufstr(&it, end);
+	if(hdr->desc == NULL) return -ENODATA;
+	if(skip_null(&it, end)) return -ENODATA;
+
+	//load author
+	hdr->author = bufstr(&it, end);
+	if(hdr->author == NULL) return -ENODATA;
+	if(skip_null(&it, end)) return -ENODATA;
+
+	//load addon version
+	if(bufcpy(&it, end, &hdr->addon_version, 4)) return -ENODATA;
+
+	hdr->length = it - begin;
+	return 0;
+}
+
+int gma_header_parse(struct gma_header *hdr, const void *buf, size_t count){
+	const int ret = _gma_header_parse(hdr, buf, count);
+	if(ret == 0){
+		return 0;
+	} else {
+		errno = -ret;
+		return -1;
+	}
+}
